0ad938adeb8a49e852e40dce55b058b9552a2c34
Rafael Guzman <rega1237@gmail.com>
2022-06-03 10:50:23 -0400

    linter fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import Header from './components/Header/Header';
 import BooksPage from './pages/BookPage/BookPage';
