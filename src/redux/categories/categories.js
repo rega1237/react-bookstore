4643363ed5be59dce42d105bedd202024a897152
Rafael Guzman <rega1237@gmail.com>
2022-06-07 08:59:39 -0400

    create redux/categories, add the reducer and action

---
+++ src/redux/categories/categories.js
@@ -0,0 +1,14 @@
+const CHECKSTATUS = 'CHECKSTATUS';
+
+export default function categoriesReducer(state = [], action) {
+  switch (action.type) {
+    case CHECKSTATUS:
+      return 'Under construction';
+    default:
+      return state;
+  }
+}
+
+export const checkSt = () => ({
+  type: CHECKSTATUS,
+});
