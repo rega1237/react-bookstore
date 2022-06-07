8b4876390f6c98cbab0afe56e96caf80ab5aedb8
Rafael Guzman <rega1237@gmail.com>
2022-06-07 18:17:10 -0400

    use provide, useSelector and default book to display in the book store

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/configureStore';
 import Header from './components/Header/Header';
 import BooksPage from './pages/BookPage/BookPage';
 import CategoriesPage from './pages/CategoriesPage/CategoriesPage';
@@ -7,15 +9,17 @@ import CategoriesPage from './pages/CategoriesPage/CategoriesPage';
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Switch>
-        <Route path="/" exact>
-          <BooksPage />
-        </Route>
-        <Route path="/categories">
-          <CategoriesPage />
-        </Route>
-      </Switch>
+      <Provider store={store}>
+        <Header />
+        <Switch>
+          <Route path="/" exact>
+            <BooksPage />
+          </Route>
+          <Route path="/categories">
+            <CategoriesPage />
+          </Route>
+        </Switch>
+      </Provider>
     </div>
   );
 }
