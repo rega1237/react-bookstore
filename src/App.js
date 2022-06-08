04a3ca7ee3aaf019a1c303c3a0a3c4e27de86a9a
Rafael Guzman <rega1237@gmail.com>
2022-06-07 20:35:20 -0400

    change provider to the index file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './redux/configureStore';
 import Header from './components/Header/Header';
 import BooksPage from './pages/BookPage/BookPage';
 import CategoriesPage from './pages/CategoriesPage/CategoriesPage';
@@ -9,17 +7,15 @@ import CategoriesPage from './pages/CategoriesPage/CategoriesPage';
 function App() {
   return (
     <div className="App">
-      <Provider store={store}>
-        <Header />
-        <Switch>
-          <Route path="/" exact>
-            <BooksPage />
-          </Route>
-          <Route path="/categories">
-            <CategoriesPage />
-          </Route>
-        </Switch>
-      </Provider>
+      <Header />
+      <Switch>
+        <Route path="/" exact>
+          <BooksPage />
+        </Route>
+        <Route path="/categories">
+          <CategoriesPage />
+        </Route>
+      </Switch>
     </div>
   );
 }
