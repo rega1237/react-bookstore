71ad09949d5d9986d32a58a59cfddcddb7b5022a
Rafael Guzman <rega1237@gmail.com>
2022-06-09 22:03:35 -0400

    added style to the bookstore

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Book.css';
 import { useDispatch } from 'react-redux';
+import { CircularProgressBar } from '@tomik23/react-circular-progress-bar';
 import { delFetchBook } from '../../redux/books/books';
 
 const Book = (props) => {
@@ -13,34 +14,42 @@ const Book = (props) => {
   const delBookHandler = () => {
     dispatch(delFetchBook(id));
   };
+
   return (
     <div className="book-container">
       <div className="book-data">
-        <h4 className="book-type">ACTION</h4>
+        <h4 className="book-type">Action</h4>
         <h2 className="book-name">{title}</h2>
         <h4 className="book-author">{author}</h4>
         <ul className="book-action">
           <li>
-            <button type="button">Comments</button>
+            <button className="btn-action" type="button">Comments</button>
           </li>
           <li>
-            <button type="button" onClick={delBookHandler}>Remove</button>
+            <button className="btn-action" type="button" onClick={delBookHandler}>Remove</button>
           </li>
           <li>
-            <button type="button">Edit</button>
+            <button className="btn-action" type="button">Edit</button>
           </li>
         </ul>
       </div>
       <div className="book-track">
         <div className="book-percentage">
-          <h3>64%</h3>
-          <p>Completed</p>
+          <CircularProgressBar
+            percent={64}
+            size={100}
+            number={false}
+          />
+          <div className="book-percentage-data">
+            <h3>64%</h3>
+            <p>Completed</p>
+          </div>
         </div>
       </div>
       <div className="book-read">
         <p>Current Chapter</p>
         <h3>Chapter 17</h3>
-        <a href="/otherpage">UPDATE PROGRESS</a>
+        <button type="button" className="btn-update">UPDATE PROGRESS</button>
       </div>
     </div>
   );
